906ab6c738a56c90d05cb9db404e6473638af250
prahlad2200 <prahlad2200@gmail.com>
2018-01-06 16:45:50 +0530

    add array-declaration php- prahlad2200

---
+++ array-declaration.php
@@ -0,0 +1,20 @@
+<?php 
+/* --------------------- */
+/*  Prahlad Makwana      */
+/* --------------------- */
+
+$array=[];
+/*
+$array=array();
+*/
+var_dump($array);
+
+$array[]='one';
+$array[]='two';
+$array[]='three';
+/*
+$array=array('one','two','three');
+*/
+
+var_dump($array);
+?>
